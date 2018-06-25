07dc22e7a69690e872bb068fe644b6facf087b4f
bdrum <bdrum@jinr.ru>
2018-06-25 18:33:24 +0300

    Initial commit:
    Attempt to get the access to owen device.
    Basics descriptions was added.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using OwenioNet;
+
+
+namespace rsController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // стандартный интерфейс взаимодействия через ком порты
+            string portName = "COM3";
+            SerialPort serialPort = new SerialPort();
+            //serialPort.BaudRate = 115200;
+            ////serialPort.ReadTimeout = 500;
+            //serialPort.DtrEnable = false;
+            //serialPort.RtsEnable = true;
+            try
+            {
+                serialPort.PortName = portName;
+                serialPort.Open();
+                // использование OwenioNet
+                var owenProtocol = OwenProtocolMaster.Create(new SerialPort("COM3", 9600, Parity.Even, 5, StopBits.One));  // не открывает порт
+                owenProtocol.OpenSerialPort(3, 9600, Parity.Even, 5, StopBits.One); // не открывает порт
+                if (serialPort.IsOpen)
+                    owenProtocol.OwenWrite(0x265, OwenioNet.Types.AddressLengthType.Bits11, "ab.L", new byte[] { 0x45, 0x87 });//без индекса 
+                //owenProtocol.OwenRead(0x265, OwenioNet.Types.AddressLengthType.Bits11, "ab.L");//без индекса
+                //owenProtocol.OwenWrite(0x265, OwenioNet.Types.AddressLengthType.Bits11, "ab.L", new byte[] { 0x45, 0x87 }, 0x3456);//с индексом
+                owenProtocol.CloseSerialPort();
+                Console.WriteLine("open");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SampleTransporter
-Software for transport samples to reactor
+
+**Интерфейс для управления перемещения образцов в процессе** излучения.
+
+### Подсказки по гит:
+1. Основной репозитоерий - https://github.com/regata-jinr/SampleTransporter
+2. Скачать гит для винды - https://git-scm.com/download/win
+3. Завести аккаунт тут - https://github.com
+4. Работаем так - клонируем себе копию, там делаем все что угодно.
+5. Основы гит - https://git-scm.com/book/ru/v2
+
+### Взаимодействие с owen:
+1. Библиотека для работы в среде .NET - http://www.owen.ru/uploads/opisanie_biblioteki_owenionet.pdf
+2. Руководство по эксплуатации МУ110-32Р - http://www.owen.ru/uploads/re_mu110-32r_1802.pdf
+3. Преобразователь интерфейсов RS-485 – USB. Краткое руководство - http://www.owen.ru/uploads/kr_ac4_m01__031.pdf
